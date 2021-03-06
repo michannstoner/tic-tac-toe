0b76bfe1cb49ebbea6f3f87d497c59a9c0829853
Your Name <stoner.michann@gmail.com>
2021-03-05 20:07:52 -0700

    Add event listener to game grid and create place token functionality

--- src/game.js
+++ src/game.js
@@ -18,6 +18,7 @@ class Game {
   
   }
   checkWinner() {
+  
 
   }
   resetBoard() {
--- src/main.js
+++ src/main.js
@@ -1,5 +1,25 @@
 // ~~VARIABLES + QUERY SELECTORS~~
 var game = new Game();
-var gameGrid = document.querySelectorAll('.square');
+var gameGrid = document.querySelector('#game-play-grid')
 var playerTurn = document.querySelector('#turn-display');
+var squares = document.querySelectorAll('.square');
 var winner = document.querySelector('#winner-display');
+
+gameGrid.addEventListener('click', placeToken);
+
+
+
+function placeToken(event) {
+var squareClicked = event.target;
+if (squareClicked.innerText === '') {
+   return squareClicked.innerText = `${game.currentPlayer.token}`; 
+  }
+};
+
+function show(element) {
+  element.classList.remove('hidden');
+};
+
+function hide(element) {
+  element.classList.add('hidden');
+}
